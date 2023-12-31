d8eb4a857b17d4dce29e005a99480ff5ab549f8c
Marco (Urco) <124961345+Urcowscki@users.noreply.github.com>
2023-12-31 02:34:38 -0300

    new changes
    some changes in the main.js, add data.js and started the cart part

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: 'ashuashu',
+      name: 'Casual Shirt',
+      value: 45,
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing',
+      img: 'imgs/img-1.jpg',
+    },
+    {
+      id: 'jhsajhsajhsa',
+      name: 'Office Shirt',
+      value: 70,
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing',
+      img: 'imgs/img-2.jpg',
+    },
+    {
+      id: 'vcmnvcnmvcmnc',
+      name: 'Street Shirt',
+      value: 80,
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing',
+      img: 'imgs/img-3.jpg',
+    },
+    {
+      id: 'oitriortoirt',
+      name: 'Mens Suit',
+      value: 250,
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing',
+      img: 'imgs/img-4.jpg',
+    },
+  ];
